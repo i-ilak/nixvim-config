8f2c4912912699cb44784afe0bfc59a11326bc06
Ivan Ilak <ivan.ilak@hotmail.com>
2025-01-14 13:58:43 +0100

    Clangd + Utility

---
+++ config/plugins/editor/cmake-tools.nix
@@ -0,0 +1,41 @@
+_: {
+  plugins.cmake-tools = {
+    enable = true;
+    settings = {
+      cmake_soft_link_compile_commands = true;
+      cmake_use_preset = true;
+      cmake_generate_options = {
+        "-DCMAKE_EXPORT_COMPILE_COMMANDS" = 1;
+      };
+    };
+  };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<F7>";
+      action = "<cmd>CMakeBuild<CR>";
+      options = {
+        silent = true;
+        desc = "CMake build target";
+      };
+    }
+    {
+      mode = "n";
+      key = "<F6>";
+      action = "<cmd>CMakeGenerate<CR>";
+      options = {
+        silent = true;
+        desc = "CMake generate";
+      };
+    }
+    {
+      mode = "n";
+      key = "<C-F7>";
+      action = "<cmd>CMakeBuildCurrentFile<CR>";
+      options = {
+        silent = true;
+        desc = "CMake build current file";
+      };
+    }
+  ];
+}
