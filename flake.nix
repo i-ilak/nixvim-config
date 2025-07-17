1472c9876d5aa4a5551a998fd28e47d9318336b7
Ivan Ilak <ivan.ilak@hotmail.com>
2025-07-17 23:14:08 +0200

    Pin nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,9 @@
   description = "i-ilak's nixvim config";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    # Fix nixpkgs-unstable to commit from 17.07.25, since we need some changes from
+    # after 25.05, but dont want to constantly pull unstable
+    nixpkgs.url = "github:nixos/nixpkgs/e139aa6a2b5f1f42d682a1fbc60abd355d2b4771";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
